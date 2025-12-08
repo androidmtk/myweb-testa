dc45264b77a14d23de5d033c563a021a85b0343a
androidmtk <android_sw@163.com>
2025-12-09 03:55:53 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,12 +24,12 @@ const config = {
   url: 'https://androidmtk.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/myweb-testb/',
+  baseUrl: '/myweb-testa/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'androidmtk', // Usually your GitHub org/user name.
-  projectName: 'myweb-testb', // Usually your repo name.
+  projectName: 'myweb-testa', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
