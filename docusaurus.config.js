4a1dafe558cc0d9921d6c68f07d31e643eadd546
androidmtk <android_sw@163.com>
2025-12-09 03:53:40 +0800

    Update Docusaurus config for new site settings

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,15 +20,16 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  // url: 'https://your-docusaurus-site.example.com',
+  url: 'https://androidmtk.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/myweb-testb/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'androidmtk', // Usually your GitHub org/user name.
+  projectName: 'myweb-testb', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
